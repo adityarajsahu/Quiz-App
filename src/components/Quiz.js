b401d8b2d0999851c107939a811ed55835882310
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-03-06 23:15:05 +0530

    header component added

---
+++ src/components/Quiz.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const Quiz = () => {
+    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    return <p>Current Active Question</p>;
+};
+
+export default Quiz;
