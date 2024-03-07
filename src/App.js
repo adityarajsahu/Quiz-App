ad523aab50aec5137a3ff1c0fb9d3b75bc85ad21
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-03-07 18:08:39 +0530

    displaying questions and registering answers done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+
 import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
-    return <Header />;
+    return (
+        <React.Fragment>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,32 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions";
+
 const Quiz = () => {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
 
-    return <p>Current Active Question</p>;
+    const activeQuestionIndex = userAnswers.length;
+
+    const handleSelectAnswer = (selectedAnswer) => {
+        setUserAnswers((prevAnswers) => {
+            return [...prevAnswers, selectedAnswer];
+        });
+    };
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+                        <li key={index} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Quiz;
