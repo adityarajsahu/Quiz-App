f08f870b55df5bfe761933d4fba29c0deaa7cdc7
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-03-08 21:31:54 +0530

    clear interval, clear timeout and reset progress bar done

--- src/components/QuestionTimer.js
+++ src/components/QuestionTimer.js
@@ -4,13 +4,23 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
     const [remainingTime, setRemainingTime] = useState(timeout);
 
     useEffect(() => {
-        setTimeout(() => onTimeout, timeout);
+        console.log("Setting timeout");
+        const timer = setTimeout(onTimeout, timeout);
+
+        return () => {
+            clearTimeout(timer);
+        };
     }, [timeout, onTimeout]);
 
     useEffect(() => {
-        setInterval(() => {
+        console.log("Setting interval");
+        const interval = setInterval(() => {
             setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
         }, 100);
+
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
 
     return <progress id="question-time" max={timeout} value={remainingTime} />;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
@@ -10,11 +10,13 @@ const Quiz = () => {
     const activeQuestionIndex = userAnswers.length;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-    const handleSelectAnswer = (selectedAnswer) => {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevAnswers) => {
             return [...prevAnswers, selectedAnswer];
         });
-    };
+    }, []);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
         return (
@@ -31,7 +33,7 @@ const Quiz = () => {
     return (
         <div id="quiz">
             <div id="question">
-                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
+                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer, index) => (
