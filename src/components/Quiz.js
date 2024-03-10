8fb6e525313cb5c850090eb4d579d89f72c15bf5
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-03-10 23:44:32 +0530

    quiz result summary

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,9 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question";
+
+import Summary from "./Summary";
 
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -19,12 +20,7 @@ const Quiz = () => {
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
-        return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy Icon" />
-                <h2>Quiz Completed!</h2>
-            </div>
-        );
+        return <Summary userAnswers={userAnswers} />;
     }
 
     return (
